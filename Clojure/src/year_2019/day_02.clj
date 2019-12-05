5e2a659cc0602b30d1fe3d2efb727bcbe02aed05
Dominic Chui <dominic.chui@outlook.com>
2019-12-04 21:39:13 -0500

    day 2 done

--- Clojure/src/year_2019/day_02.clj
+++ Clojure/src/year_2019/day_02.clj
@@ -12,14 +12,14 @@
 (defn get-at-index
   {:test (fn []
            (is= (get-at-index [0, 1, 2, 3, 4] 0) 0)
-           (is= (get-at-index [1, 0, 0, 0, 99] 4 ) 99)
+           (is= (get-at-index [1, 0, 0, 0, 99] 4) 99)
            (is= (get-at-index [0, 1, 2, 3, 4] 0 2) 2)
            (is= (get-at-index [1, 0, 0, 0, 99] 0 3) 0)
            )}
   ([program pointer]
-  (nth program pointer))
+   (nth program pointer))
   ([program pointer offset]
-  (nth program (+ pointer offset))))
+   (nth program (+ pointer offset))))
 
 (defn process-program
   {:test (fn []
@@ -29,22 +29,41 @@
            (is= (process-program [1, 1, 1, 4, 99, 5, 6, 0, 99]) [30, 1, 1, 4, 2, 5, 6, 0, 99])
            (is= (process-program [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]) [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50])
            )}
-  [program]
-  (loop [program program
-         pointer 0]
-    (let [opcode (nth program pointer)]
-      (if (= opcode 99)
-        program
-        (recur (cond (= opcode 1)
-                     (assoc program (get-at-index program pointer 3)
-                                    (+ (get-at-index program (get-at-index program pointer 1))
-                                       (get-at-index program (get-at-index program pointer 2))))
-                     (= opcode 2)
-                     (assoc program (get-at-index program pointer 3)
-                                    (* (get-at-index program (get-at-index program pointer 1))
-                                       (get-at-index program (get-at-index program pointer 2))))
-                     :else (error "invalid opcode " opcode))
-               (+ pointer 4))))))
+  ([program]
+   (loop [program program
+          pointer 0]
+     (let [opcode (nth program pointer)]
+       (if (= opcode 99)
+         program
+         (recur (cond (= opcode 1)
+                      (assoc program (get-at-index program pointer 3)
+                                     (+ (get-at-index program (get-at-index program pointer 1))
+                                        (get-at-index program (get-at-index program pointer 2))))
+                      (= opcode 2)
+                      (assoc program (get-at-index program pointer 3)
+                                     (* (get-at-index program (get-at-index program pointer 1))
+                                        (get-at-index program (get-at-index program pointer 2))))
+                      :else (error "invalid opcode " opcode))
+                (+ pointer 4))))))
+  ([program noun verb]
+   (let [program (-> program
+                     (assoc 1 noun)
+                     (assoc 2 verb))]
+     (loop [program program
+            pointer 0]
+       (let [opcode (nth program pointer)]
+         (if (= opcode 99)
+           program
+           (recur (cond (= opcode 1)
+                        (assoc program (get-at-index program pointer 3)
+                                       (+ (get-at-index program (get-at-index program pointer 1))
+                                          (get-at-index program (get-at-index program pointer 2))))
+                        (= opcode 2)
+                        (assoc program (get-at-index program pointer 3)
+                                       (* (get-at-index program (get-at-index program pointer 1))
+                                          (get-at-index program (get-at-index program pointer 2))))
+                        :else (error "invalid opcode " opcode))
+                  (+ pointer 4))))))))
 
 (defn puzzle-2a
   {:test (fn []
@@ -52,7 +71,17 @@
            )}
   []
   (-> (get-input)
-      (assoc 1 12)
-      (assoc 2 2)
-      (process-program)))
+      (process-program 12 2)))
 
+(defn puzzle-2b
+  {:test (fn []
+           (is= (puzzle-2b) 6577)
+           )}
+  []
+  (let [input (get-input)]
+    (as-> (for [noun (range 100)
+                verb (range 100)] [noun, verb]) $
+          (map (fn [tuple] (process-program input (first tuple) (last tuple))) $)
+          (filter (fn [program-result] (= (first program-result) 19690720)) $)
+          (first $)
+          (+(* 100 (nth $ 1)) (nth $ 2)))))
